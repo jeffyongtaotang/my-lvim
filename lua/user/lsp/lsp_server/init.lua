b94db1d67f51966a66708e1d210bce74ee2542da
jeffyongtaotang <jeff.tong@konghq.com>
2023-01-23 15:21:22 -0700

    chore(yamlls) add schema support

--- lua/user/lsp/lsp_server/init.lua
+++ lua/user/lsp/lsp_server/init.lua
@@ -3,7 +3,8 @@ require("lspconfig")["yamlls"].setup({
     yaml = {
       schemas = {
         -- kol configuration schema
-        ["/Users/jeff.tong@konghq.com/Coding/konnect-on-call-tool-kit/cmd/local-dev/schemas/configuration.schema.yaml"] = "*.kol.config.yaml"
+        ["/Users/jeff.tong@konghq.com/Coding/konnect-on-call-tool-kit/cmd/local-dev/schemas/configuration.schema.yaml"] = "*.kol.config.yaml",
+        ["http://json-schema.org/draft-07/schema"] = "/cmd/local-dev/**/*.schema.yaml",
       }
     }
   }
