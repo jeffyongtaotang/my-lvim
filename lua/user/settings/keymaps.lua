25edb1ef0f3f37ad8b984fcdcbb054f199f68b2a
jeffyongtaotang <jeff.tong@konghq.com>
2023-01-23 23:00:41 -0700

    refactor(term_mode) rm terminal mode switching key maps
    Remove terminal mode switch keymap, to let the zsh vim key binding
    works.

--- lua/user/settings/keymaps.lua
+++ lua/user/settings/keymaps.lua
@@ -1,9 +1,6 @@
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
--- Change to nornal mode from ternminal mode
-lvim.keys.term_mode["<esc>"] = "<C-\\><C-n>"
-
 local vgit_which_keys = require("user.plugin_cfg.vgit").get_which_key()
 local octo_which_keys = require("user.plugin_cfg.octo_cfg").get_which_key()
 
