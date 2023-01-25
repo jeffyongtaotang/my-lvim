63d28c34a59434a2a7b244576835556b6bd9653c
jeffyongtaotang <jeff.tong@konghq.com>
2023-01-24 22:31:15 -0700

    chore(telescope) show abs file path

--- lua/user/plugin_cfg/telescope.lua
+++ lua/user/plugin_cfg/telescope.lua
@@ -1,6 +1,12 @@
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
+
+lvim.builtin.telescope.defaults = {
+  -- show abs file path
+  path_display = { "absolute" },
+}
+
 lvim.builtin.telescope.defaults.mappings = {
   -- for input mode
   i = {
