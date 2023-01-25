56391fb92fb878d60f9ce48e977c8102716b7d28
jeffyongtaotang <jeff.tong@konghq.com>
2023-01-25 00:49:11 -0700

    add ghNvim

--- lua/user/settings/keymaps.lua
+++ lua/user/settings/keymaps.lua
@@ -2,11 +2,9 @@
 lvim.leader = "space"
 
 local vgit_which_keys = require("user.plugin_cfg.vgit").get_which_key()
-local octo_which_keys = require("user.plugin_cfg.octo_cfg").get_which_key()
 
 lvim.builtin.which_key.mappings["g"] = {
   name = "+Git",
   g = vgit_which_keys.g,
   f = vgit_which_keys.f,
-  p = octo_which_keys.p,
 }
